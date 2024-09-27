7b489ac98ead0a8c17c94961d9d6c216b87c13f8
Shree Prajan <shreeprajan17@gmail.com>
2024-09-27 21:37:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,25 +4,25 @@ import joblib
 import re
 from nltk.corpus import stopwords
 
-# Load your pre-trained model and vectorizer
-model = joblib.load('sentiment_model.pkl')  # Load the model
-vectorizer = joblib.load('tfidf_vectorizer.pkl')  # Load the vectorizer
+# Loading pre-trained model and vectorizer
+model = joblib.load('sentiment_model.pkl')
+vectorizer = joblib.load('tfidf_vectorizer.pkl')
 
 
 # Function to preprocess the tweet
 def preprocess_new_tweet(text):
-    text = re.sub(r'@[A-Za-z0-9]+', '', text)  # Remove @mentions
-    text = re.sub(r'http\S+|www.\S+', '', text)  # Remove URLs
-    text = re.sub(r'[^a-zA-Z\s]', '', text)  # Remove special characters and numbers
-    text = text.lower()  # Convert text to lowercase
-    text = ' '.join([word for word in text.split() if word not in stopwords.words('english')])  # Remove stopwords
+    text = re.sub(r'@[A-Za-z0-9]+', '', text) 
+    text = re.sub(r'http\S+|www.\S+', '', text)
+    text = re.sub(r'[^a-zA-Z\s]', '', text)
+    text = text.lower()
+    text = ' '.join([word for word in text.split() if word not in stopwords.words('english')])
     return text
 
 
 # Function to predict sentiment
 def predict_sentiment(tweet):
     cleaned_tweet = preprocess_new_tweet(tweet)
-    tweet_tfidf = vectorizer.transform([cleaned_tweet])  # Use the loaded TF-IDF vectorizer
+    tweet_tfidf = vectorizer.transform([cleaned_tweet])
     prediction = model.predict(tweet_tfidf)
     return "Positive" if prediction == 1 else "Negative"
 
